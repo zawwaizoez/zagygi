dc8bfe91c350e476f75c7eec70847b3b116ea618
zawwaizoe <rohinjar456+1@gmail.com>
2017-11-07 00:48:33 -0800

    Initial commit

---
+++ app/src/main/java/com/example/kryptonite/simpleblogs/Main.java
@@ -0,0 +1,32 @@
+package com.example.kryptonite.simpleblogs;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.action_add){
+            startActivity(new Intent(Main.this,PostActivity.class));
+        }
+
+
+        return  super.onOptionsItemSelected(item);
+    }
+}
